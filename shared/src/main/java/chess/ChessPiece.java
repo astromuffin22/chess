9b6d28470d6c4bd63f2300329bfa3db7905b5876
astromuffin22 <156139316+astromuffin22@users.noreply.github.com>
2025-01-18 19:15:32 -0500

    moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -56,21 +56,37 @@ public class ChessPiece {
 
         switch (pieceType) {
             case KING:
+                addKingMoves(board, myPosition, moves);
                 break;
             case QUEEN:
+                addQueenMoves(board, myPosition, moves);
                 break;
             case BISHOP:
+                addBishopMoves(board, myPosition, moves);
                 break;
             case KNIGHT:
+                addKnightMoves(board, myPosition, moves);
                 break;
             case ROOK:
+                addRookMoves(board, myPosition, moves);
                 break;
             case PAWN:
+                addPawnMoves(board, myPosition, moves);
                 break;
 
 
         }
 
         return moves;
+    }
+    private void addKingMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+        for (int[] direction : directions) {
+            addMoveIfValid(board, myPosition, direction[0], direction[1], moves, false);
+        }
+    }
+    private void addQueenMoves(ChessBoard board, ChessPosition myPosition, Collection<ChessMove> moves) {
+        addRookMoves(board, myPosition, moves);
+        addBishopMoves(board, myPosition, moves);
     }
 }
